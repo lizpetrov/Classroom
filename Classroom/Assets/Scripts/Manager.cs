4cecee5faa566c74d6a8d0820dc7edc4e8e48c61
xavierlu <xllgms@gmail.com>
2016-08-11 15:19:59 -0400

    update registration

--- Classroom/Assets/Scripts/Manager.cs
+++ Classroom/Assets/Scripts/Manager.cs
@@ -3,20 +3,28 @@ using System.Collections;
 
 public class Manager : MonoBehaviour {
 
-	private string key;
-	public string addEntryURL = "";
-	public string getEntryURL = "";
+	private static string key = "mySecretKey";
+	public static string addEntryURL = "http://www.classdiscover.com/register.php?";
+	public static string loginURL = "http://www.classdiscover.com/login.php?";
+	public static string getEntryURL = "";
 
 	// Use this for initialization
 	void Start () {
-		StartCoroutine (AddEntry("Xavier", "Lu", 11, 1, "Horn", "hi@gmail.com"));
+		//StartCoroutine ();
 	}
 
-	IEnumerator AddEntry(string firstName, string lastName, int grade, int periodNumber, string teacherName, string email){
-		string hash = MD5.Md5Sum (firstName + lastName + grade + periodNumber + teacherName + email + key);
-		string postURL = addEntryURL + "firstName=" + WWW.EscapeURL(firstName) + "&lastName=" + WWW.EscapeURL(lastName) + "&grade=" + grade + 
-							"&periodNumber=" + periodNumber + "&teacherName=" + WWW.EscapeURL(teacherName) + 
-							"&email=" + WWW.EscapeURL(email) + "&hash=" + hash;
+	public static IEnumerator Register(string firstName, string lastName, string email, string password, int grade, string gender, 
+						string period1TeacherName, string period2TeacherName, string period3TeacherName, 
+						string period4TeacherName, string period5TeacherName, string period6TeacherName){
+
+		string hash = MD5.Md5Sum (firstName + lastName + email + password + key);
+		string postURL = addEntryURL + "firstName=" + WWW.EscapeURL(firstName) + "&lastName=" + WWW.EscapeURL(lastName) + 
+										"&email=" + WWW.EscapeURL(email) + "&password=" + WWW.EscapeURL(password) + 
+										"&grade=" + grade + "&gender=" + gender + 
+										"&period1TeacherName=" + period1TeacherName + "&period2TeacherName=" + period2TeacherName + 
+										"&period3TeacherName=" + period3TeacherName + "&period4TeacherName=" + period4TeacherName + 
+										"&period5TeacherName=" + period5TeacherName + "&period6TeacherName=" + period6TeacherName + 
+							 			"&hash=" + hash;
 
 		WWW post = new WWW (postURL);
 		yield return post;
@@ -25,6 +33,25 @@ public class Manager : MonoBehaviour {
 			Debug.LogError ("Error: "+post.error);
 		}
 	}
+
+	IEnumerator Login(string email, string password){
+		string url = loginURL + "email=" + WWW.EscapeURL (email);
+		WWW loginUser = new WWW (url);
+		yield return loginUser;
+
+		if (loginUser.error != null) {
+			Debug.LogError ("Error: " + loginUser.error);
+		} 
+		else {
+			if (password.Equals (loginUser.text)) {
+				//success
+			} 
+			else {
+				Debug.LogError ("Email/Password incorrect!");
+			}
+		}
+	}
+
 
 	IEnumerator GetStudentsInClass(int periodNumber, string teacherName){
 		string hash = MD5.Md5Sum (periodNumber + teacherName + key);
--- Classroom/Assets/Scripts/UIManager.cs
+++ Classroom/Assets/Scripts/UIManager.cs
@@ -6,12 +6,32 @@ public class UIManager : MonoBehaviour {
 
 	public Canvas mainCanvas;
 	public Canvas registerCanvas;
-	public Text creditText;
-	public Toggle[] genderToggles;
+
+	//register
+	public InputField firstname;
+	public InputField lastname;
+	public InputField email;
+	public InputField password;
+	public InputField grade;
+	public Toggle male;
+	public Toggle female;
+	public Toggle hotpotato;
+	public InputField[] periodTeachers;
 
 	// Use this for initialization
 	void Start () {
-	
+		
+	}
+
+	void Update(){
+		if (registerCanvas.enabled) {
+			if (male.isOn && female.isOn)
+				male.isOn = false;
+			else if (male.isOn && hotpotato.isOn)
+				hotpotato.isOn = false;
+			else if (female.isOn && hotpotato.isOn)
+				hotpotato.isOn = false;
+		}
 	}
 
 	public void OpenRegisterPage(){
@@ -20,11 +40,21 @@ public class UIManager : MonoBehaviour {
 	}
 
 	public void SubmitRegister(){
+		string gender;
+		if(male.isOn)
+			gender = "M";
+		else if(female.isOn)
+			gender = "F";
+		else if(hotpotato.isOn)
+			gender = "HP";
+		else
+			gender = "F";
+
+		Manager.Register (firstname.text, lastname.text, email.text, password.text, int.Parse(grade.text.Trim()), gender,
+			periodTeachers[0].text, periodTeachers[1].text, periodTeachers[2].text, periodTeachers[3].text, 
+			periodTeachers[4].text, periodTeachers[5].text);
+
 		mainCanvas.enabled = true;
 		registerCanvas.enabled = false;
-	}
-
-	public void ToggleCreditText(){
-		creditText.enabled = !creditText.enabled;
 	}
 }
